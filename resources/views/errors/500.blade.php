d998d3844f5e711fa7a7fb9c2ea2c8c8d86b13c7
ahmedqo <ahmedqo1995@gmail.com>
2024-04-20 22:26:19 +0100

    ccc

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,14 +1,14 @@
 @extends('shared.guest.base')
-@section('title', __('Page Not Found'))
+@section('title', __('Something Went Wrong'))
 
 @section('content')
     <section class="w-full relative container mx-auto p-4 flex flex-col gap-4 lg:gap-8 my-4 lg:my-10">
         <div class="w-full flex flex-col items-center gap-6 md:gap-10">
-            <img src="{{ asset('img/svg/404.svg') }}?v={{ env('APP_VERSION') }}" alt="ItalMenara not found image"
+            <img src="{{ asset('img/svg/500.svg') }}?v={{ env('APP_VERSION') }}" alt="ItalMenara not found image"
                 loading="lazy" width="100%" height="auto"
                 class="block w-8/12 sm:w-7/12 lg:w-1/3 mx-auto pointer-events-none">
             <h1 class="uppercase font-x-huge text-x-black text-3xl lg:text-4xl">
-                {{ ucwords(__('Page Not Found')) }}
+                {{ ucwords(__('Something Went Wrong')) }}
             </h1>
         </div>
     </section>
